dea4b9782dc740b36d26c676018b7102986ef961
caevee <pister.alex.cv@gmail.com>
2020-07-26 16:46:09 +0200

    Updates

---
+++ precache-manifest.5240fff7b3821ca9d921ed8be8d9e274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b94546ec570779a0b66463d848427d9",
+    "url": "/crossnotes-frontend/index.html"
+  },
+  {
+    "revision": "6ff184c479812232fb48",
+    "url": "/crossnotes-frontend/static/css/main.81a7a52c.chunk.css"
+  },
+  {
+    "revision": "e2ea0468ce7d0bc62cea",
+    "url": "/crossnotes-frontend/static/js/2.3918c2c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crossnotes-frontend/static/js/2.3918c2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff184c479812232fb48",
+    "url": "/crossnotes-frontend/static/js/main.ce1b2c1e.chunk.js"
+  },
+  {
+    "revision": "adbaca2dadf50c399892",
+    "url": "/crossnotes-frontend/static/js/runtime-main.cffb80fc.js"
+  }
+]);
